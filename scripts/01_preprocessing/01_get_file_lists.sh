cd9493801595e3c6b83fcdbdd9a43b803b73e0af
kenneditodd <kenneditodd@gmail.com>
2023-12-13 20:05:24 -0600

    add preprocessing scripts

---
+++ scripts/01_preprocessing/01_get_file_lists.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get fastq file list
+# There are ? fastq files per sample
+# Each sample has R1 and R2 on all ? lanes
+cd /research/labs/neurology/fryer/projects/psilocybin/psil1
+out=/research/labs/neurology/fryer/m214960/psilocybin/refs/fastq_file_list.txt
+ls -1 | grep .fastq.gz > $out
+
+# Get sample file list
+cd /research/labs/neurology/fryer/projects/psilocybin/psil1
+out=/research/labs/neurology/fryer/m214960/psilocybin/refs/sample_file_list.txt
+ls -1 | grep _L001_R1_001.fastq.gz > $out
