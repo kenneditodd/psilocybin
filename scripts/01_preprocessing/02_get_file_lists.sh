d5c0b04fd446edf54d78f2ef19259b3877b927e5
kenneditodd <kenneditodd@gmail.com>
2023-12-19 14:22:38 -0600

    change paths

--- scripts/01_preprocessing/02_get_file_lists.sh
+++ scripts/01_preprocessing/02_get_file_lists.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # Get fastq file list
-# There are ? fastq files per sample
-# Each sample has R1 and R2 on all ? lanes
-cd /research/labs/neurology/fryer/projects/psilocybin/psil1
-out=/research/labs/neurology/fryer/m214960/psilocybin/refs/fastq_file_list.txt
+# There are 2 fastq files per sample
+cd ../../rawReads
+out=../refs/fastq_file_list.txt
 ls -1 | grep .fastq.gz > $out
 
 # Get sample file list
-cd /research/labs/neurology/fryer/projects/psilocybin/psil1
-out=/research/labs/neurology/fryer/m214960/psilocybin/refs/sample_file_list.txt
+out=../refs/sample_file_list.txt
 ls -1 | grep _R1_ > $out
