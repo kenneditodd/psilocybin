85f1b7a92d0391d5faff5ab721d15961a7b16c4e
kenneditodd <kenneditodd@gmail.com>
2025-06-24 11:21:15 -0700

    update web summary path

--- snRNAseq/scripts/01_Cell_Ranger/04_get_web_summary.sh
+++ snRNAseq/scripts/01_Cell_Ranger/04_get_web_summary.sh
@@ -5,7 +5,7 @@ source ../../refs/.env
 
 # extract webs summaries
 while read -r sample; do
-	cd $PROJECT_DIR/counts/$sample/outs
-	cp web_summary.html ../../web_summaries/"$sample"_web_summary.html
+	cd $PROJECT_DIR/counts/$sample/outs/per_sample_outs/$sample
+	cp web_summary.html ../../../../web_summaries/"$sample"_web_summary.html
 done < $PROJECT_DIR/refs/sample_list.tsv
 
