435c43fa61ce0cd11a90a9872fc503afcf451233
kenneditodd <kenneditodd@gmail.com>
2025-06-24 11:18:44 -0700

    update metrics

--- snRNAseq/scripts/01_Cell_Ranger/05_get_metrics.R
+++ snRNAseq/scripts/01_Cell_Ranger/05_get_metrics.R
@@ -2,6 +2,8 @@
 library(stringr)
 library(dotenv)
 library(dplyr)
+library(readr)
+library(tidyr)
 
 # load the environment variables
 load_dot_env(file = "../../refs/.env")
@@ -11,45 +13,46 @@ project_dir <- Sys.getenv("PROJECT_DIR")
 
 # file locations
 samples <- readLines(paste0(project_dir, "/refs/sample_list.tsv"))
-locations <- paste0(project_dir, "/counts/", samples, "/outs/metrics_summary.csv")
+locations <- paste0(project_dir, "/counts/", samples, "/outs/per_sample_outs/", samples, "/metrics_summary.csv")
 
 # initialize df and loop through files
 df <- data.frame()
 for (i in 1:length(locations)) {
   if (i == 1) {
-    df <- read.csv(locations[i])
+    df <- readr::read_csv(locations[i], show_col_types = FALSE)
+    df <- df %>%
+      filter(`Grouped By` == "Physical library ID") %>%
+      select(`Group Name`, `Metric Name`, `Metric Value`) %>%
+      pivot_wider(names_from = `Metric Name`, values_from = `Metric Value`)
+    df$tgen_subject_name <- samples[i]
   } else {
-    row <- read.csv(locations[i])[1,]
+    row <- readr::read_csv(locations[i], show_col_types = FALSE)
+    row <- row %>%
+      filter(`Grouped By` == "Physical library ID") %>%
+      select(`Group Name`, `Metric Name`, `Metric Value`) %>%
+      pivot_wider(names_from = `Metric Name`, values_from = `Metric Value`)
+    row$tgen_subject_name <- samples[i]
+    row <- row[1,]
     df <- rbind(df,row)
   }
 }
+remove(row)
+
+# read metadata
+meta <- readRDS("../../rObjects/meta.rds")
+
+# add sample_id by joining with tgen_subject_name
+df <- left_join(x = df, y = meta[,c(1,16)], by = "tgen_subject_name")
+remove(meta)
 
 # rename rows
-rownames(df) <- samples
+df <- as.data.frame(df)
+rownames(df) <- df$sample_id
 
-# rename columns
-df <- df %>%
-  dplyr::rename(
-    estimated_cells = Estimated.Number.of.Cells,
-    mean_reads_per_cell = Mean.Reads.per.Cell,
-    median_genes_per_cell = Median.Genes.per.Cell,
-    number_reads = Number.of.Reads,
-    valid_barcodes = Valid.Barcodes,
-    sequencing_saturation = Sequencing.Saturation,
-    Q30_bases_barcode = Q30.Bases.in.Barcode,
-    Q30_bases_read = Q30.Bases.in.RNA.Read,
-    Q30_bases_UMI = Q30.Bases.in.UMI,
-    reads_mapped_genome = Reads.Mapped.to.Genome,
-    confident_reads_mapped_genome = Reads.Mapped.Confidently.to.Genome,
-    confident_intergenic_reads_mapped = Reads.Mapped.Confidently.to.Intergenic.Regions,
-    confident_intronic_reads_mapped = Reads.Mapped.Confidently.to.Intronic.Regions,
-    confident_exonic_reads_mapped = Reads.Mapped.Confidently.to.Exonic.Regions,
-    confident_reads_mapped_transcriptome = Reads.Mapped.Confidently.to.Transcriptome,
-    reads_mapped_antisense = Reads.Mapped.Antisense.to.Gene,
-    fraction_reads = Fraction.Reads.in.Cells,
-    total_genes = Total.Genes.Detected,
-    median_UMI_per_cell = Median.UMI.Counts.per.Cell
-  )
+# rename/reorder columns
+colnames(df) <- gsub(" ", "_", tolower(colnames(df)))
+df <- df[,c(18,17,2,11:16,3:10)]
+
 
 write.table(df, 
             paste0(project_dir, "/counts/web_summaries/overall_metrics.tsv"),
