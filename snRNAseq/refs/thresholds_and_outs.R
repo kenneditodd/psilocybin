88f178b18ed8a66b1c3133967e23a4569451e72b
kenneditodd <kenneditodd@gmail.com>
2025-09-09 11:35:26 -0700

    update

--- snRNAseq/refs/thresholds_and_outs.R
+++ snRNAseq/refs/thresholds_and_outs.R
@@ -1,22 +1,24 @@
 # variables
-filtering_method <- "thresh1_no_integration"
+filtering_method <- "thresh1_soupx"
 out <- paste0("../../results/", filtering_method, "/")
 
 # thresholds
-if (filtering_method == "thresh1_no_integration") {
+if (filtering_method == "thresh1_soupx") {
   # Cell Ranger output
-  nCount.min <- 5000
-  nCount.max <- 50000
+  nCount.min <- 7000
+  nCount.max <- 80000
+  nFeature.min <- 2000
+  complexity.cutoff <- 0.78
+  mt.cutoff <- 1
+  hb.cutoff <- 1
+  nuclear.cutoff <- 60
+} else if (filtering_method == "thresh2_cellbender") {
+  # cellbender output
+  nCount.min <- 7000
+  nCount.max <- 80000
   nFeature.min <- 2000
   complexity.cutoff <- 0.8
   mt.cutoff <- 1
   hb.cutoff <- 1
-} else if (filtering_method == "thresh1_cellbender") {
-  # Cell Ranger output
-  nCount.min <- 5000
-  nCount.max <- 50000
-  nFeature.min <- 2000
-  complexity.cutoff <- 0.8
-  mt.cutoff <- 1
-  hb.cutoff <- 1
-} 
+}
+
